b2a44790f257cad25f87076c6d80fd3f3029a575
jdbeltrans <72110790+jdbeltrans@users.noreply.github.com>
2021-02-17 19:44:30 -0500

    Editing the message from GitHub

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -4,7 +4,8 @@ public class AplMain {
 
     public static void main(String[] args)
     {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo, este es el curso de Git");
+        
 
     }
 }
